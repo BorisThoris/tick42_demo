deafb4380535da0c71b45c4fc8e5ec003353f295
Boris <modaxxx009@yahoo.com>
2020-06-22 03:53:54 +0300

    Updates

---
+++ precache-manifest.e987aa99b5ec7619e8f2bbd3a41f7f08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a00b701cb1428528e94c4d61c05f798",
+    "url": "/tick42_demo/index.html"
+  },
+  {
+    "revision": "e0b2194c49e2dbf71cc2",
+    "url": "/tick42_demo/static/css/main.54e6d163.chunk.css"
+  },
+  {
+    "revision": "1da3e876aaa7c6c08ad2",
+    "url": "/tick42_demo/static/js/2.9a06bdf5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tick42_demo/static/js/2.9a06bdf5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0b2194c49e2dbf71cc2",
+    "url": "/tick42_demo/static/js/main.37906449.chunk.js"
+  },
+  {
+    "revision": "2cd47722f881c225de2a",
+    "url": "/tick42_demo/static/js/runtime-main.a433237f.js"
+  }
+]);
